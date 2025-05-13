0240e1ca44e68773657e8478b1dea1727099b927
kolesnikoffone <kolesnikoff.one@gmail.com>
2025-05-13 15:08:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -128,7 +128,7 @@ async def fetch_tokens(sort_type: str, min_cap: float, limit: int = 40, paginate
                             except:
                                 growth_str = "🫥 0.00%"
 
-                            line = f"{idx}. {name_symbol} • {mcap} • {growth_str}"    
+                            line = f"{idx}.{growth_str} • {name_symbol} • {mcap}"    
                             result.append(line)
                         pages.append("\n".join(result))
 
